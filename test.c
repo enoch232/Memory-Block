d0ee4b1035e95c5e64a1fc62baf4c35af816532a
Enoch Ko <enoch232@gmail.com>
2017-04-12 12:11:49 -0700

    initalize

---
+++ test.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#include<mem.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#define MEMORY_SPACE 1024 // number of Default array size
+
+typedef struct {
+  int process_id;
+  int size;
+  int start;
+  memory_node *next;
+} memory_node;
+
+void allocateMem( int processID, int memSize ) {
+
+}
+
+void deleteMem( int processID ) {
+
+}
+
+void print() {
+  
+}
+
+
+
+
+int main() {
+
+}
