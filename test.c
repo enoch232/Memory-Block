6735c4cd862544f61334f6ae9a02567626c1502e
Enoch Ko <enoch232@gmail.com>
2017-04-12 12:14:50 -0700

    add queue

--- test.c
+++ test.c
@@ -1,17 +1,29 @@
 #include<stdio.h>
 #include<string.h>
-#include<mem.h>
+#include "mem.h"
 #include<stdlib.h>
 #include<unistd.h>
 
 #define MEMORY_SPACE 1024 // number of Default array size
 
-typedef struct {
+struct memory_node {
   int process_id;
   int size;
   int start;
-  memory_node *next;
-} memory_node;
+  struct memory_node *next;
+};
+
+struct memory_queue {
+  int process_id;
+  int size;
+  struct memory_queue *next;
+};
+
+struct memory_node *node_head = NULL;
+struct memory_node *node_current = NULL;
+
+struct memory_queue *queue_head = NULL;
+struct memory_queue *queue_current = NULL;
 
 void allocateMem( int processID, int memSize ) {
 
@@ -22,7 +34,7 @@ void deleteMem( int processID ) {
 }
 
 void print() {
-  
+
 }
 
 
